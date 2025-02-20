1f7a66e762f29cf922668978fd001c14b14695d9
lakshmi1113 <lakshmi.sharavati@gmail.com>
2025-02-20 19:23:58 +0530

    javahw_ls-1

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String[] args){
+        int a = 100;
+        int b = 250;
+        int sum = a+ b;
+        System.out.println("Let us see the addition of two numbers :- 100 and 250");
+        System.out.println("the sum of the two numbers is " + sum);    
+    }
+}
