0a878ddd43141194a47f2ebf7cb1f99b6f10f89a
lakshmi1113 <lakshmi.sharavati@gmail.com>
2025-02-25 20:29:59 +0530

    javaHW

--- grading_system.java
+++ grading_system.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
+
 public class grading_system {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter your marks:");
         int marks = sc.nextInt();
-        if(marks>90){
+        if (marks > 90) {
             System.out.println("O");
-        }
-        else if(marks>80 && marks<=90){
+        } else if (marks > 80 && marks <= 90) {
             System.out.println("A+");
-        }
-        else if(marks>70 && marks<=80){
+        } else if (marks > 70 && marks <= 80) {
             System.out.println("A");
-        }
-        else if(marks>60 && marks<=70){
+        } else if (marks > 60 && marks <= 70) {
             System.out.println("B");
-        }
-        else if(marks>= 50 && marks<60){
+        } else if (marks >= 50 && marks < 60) {
             System.out.println("C");
-        }
-        else{
+        } else {
             System.out.println("F");
         }
+        sc.close();
     }
 }
