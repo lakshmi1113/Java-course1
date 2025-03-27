b0760a82cb33f410a484fbb9b112bfb251dab946
lakshmi1113 <lakshmi.sharavati@gmail.com>
2025-03-27 18:04:33 +0530

    javacw

--- Arrays2d.java
+++ Arrays2d.java
@@ -19,5 +19,30 @@ public class Arrays2d {
                 }
                 System.out.println();
             }
+            int cmin = 0, rmin = 0, cmax = (ncol-1), rmax = (nrow-1);
+            int count = 0;
+            System.out.println("The spiral traversal is as follows:");
+            while(count < (nrow*ncol)){
+                for(int col = cmin; col <=cmax && count < nrow * ncol ; col++){
+                    System.out.print(arr[rmin][col] + " ");
+                    count++;
+                }
+                rmin ++;
+                for(int row = rmin; row<=rmax && count < nrow * ncol; row++){
+                    System.out.print(arr[row][cmax] + " ");
+                    count++;
+                }
+                cmax--;
+                for(int col = cmax; col>=cmin && count < nrow * ncol; col--){
+                    System.out.print(arr[rmax][col]+ " ");
+                    count++;
+                }        
+                rmax--;
+                for(int row = rmax; row>=rmin && count < nrow * ncol; row--){
+                    System.out.print(arr[row][cmin] + " ");
+                    count++;
+                }
+                cmin++;
+            }
         }
     }
---
+++ RemovePrime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.*;
+public class RemovePrime{
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+     public static void solution(ArrayList<Integer> a){
+            for(int i = a.size()-1; i >= 0; i--){
+                if(isPrime(a.get(i)) == true){
+                    a.remove(i);
+                }
+            }
+        }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of arraylist:");
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList();
+        for(int i = 0; i<n-1; i++){
+            arr.add(sc.nextInt());
+        }
+        solution(arr);
+        System.out.println(arr);
+    }
+    }
